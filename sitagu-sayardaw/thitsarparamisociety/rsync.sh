00bc64b4d4505a763ed23a58d5a1370a0a8c1d5a
sai <sai@gmail.com>
2018-12-04 14:23:56 +0700

    init

--- sitagu-sayardaw/thitsarparamisociety/rsync.sh
+++ sitagu-sayardaw/thitsarparamisociety/rsync.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-#rsync -avzzz -P {600..1034}.mp3 -e 'ssh -i /cygdrive/d/sms_copy/ssh_keygen/this' walen@192.168.1.123:/home/walen/mahasi/ahshinottama/
-#rsync -avzzz -P *.mp3 -e 'ssh -i /cygdrive/d/sms_copy/ssh_keygen/this' walen@192.168.1.123:/home/walen/mahasi/ahshinottama/
-#rsync -avzzz -P walen@192.168.1.123:/home/walen/mahasi/ahshinottama/{610..700}.mp4 .
-rsync -avzzz -P walen@192.168.1.123:/home/walen/mahasi/ahshinottama/*mp4 .
+#rsync -avzzz -P {600..1034}.mp3 -e 'ssh -i /cygdrive/d/sms_copy/ssh_keygen/this' user@192.168.1.123:/home/user/mahasi/ahshinottama/
+#rsync -avzzz -P *.mp3 -e 'ssh -i /cygdrive/d/sms_copy/ssh_keygen/this' user@192.168.1.123:/home/user/mahasi/ahshinottama/
+#rsync -avzzz -P user@192.168.1.123:/home/user/mahasi/ahshinottama/{610..700}.mp4 .
+rsync -avzzz -P user@192.168.1.123:/home/user/mahasi/ahshinottama/*mp4 .
