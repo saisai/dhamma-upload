e59b9989b851a0a34b87d349247c6f1a7b2d78e8
sai <sai@gmail.com>
2019-03-14 12:46:38 +0700

    init

---
+++ future/crawler/crawler/__init__.py
@@ -0,0 +1,21 @@
+
+
+from crawler.core import (
+    get_html_mp4,
+    get_html_mp3,
+    check_duplicate,
+    convert_myanmar_number,
+    get_json,    
+    update_raw_titles_links,
+)
+
+from crawler.thread_download import thread_download
+
+from crawler.thread_upload import thread_upload, thread_upload_test
+
+
+
+'''
+if __name__ == '__main__':
+    get_html(sys.argv[1])
+'''
