3daa0c8d373b0a627b3146f5674249f4ce586a19
sai <sai@gmail.com>
2019-04-18 16:23:27 +0700

    delete unused files

--- Mahasi-Sayadaw-U-Sobhana-Mahathera/dhammadownload_mp3s/rsync.sh
+++
@@ -1,3 +0,0 @@
-#sshpass -p snp rsync -avzzz -p {001..030}.mp4 -e 'ssh -p 8022' snp@192.168.1.49:/data/data/com.termux/files/home/storage/external-1/youtube/Mandalay-Yankintaung-Laykyun-Man-Aung-Sayadaw-Bhaddanta-Withuta/
-sshpass -p snp rsync -avzzz -p *.txt -e 'ssh -p 8022' snp@192.168.1.49:/data/data/com.termux/files/home/storage/external-1/youtube/Mandalay-Yankintaung-Laykyun-Man-Aung-Sayadaw-Bhaddanta-Withuta/
-
