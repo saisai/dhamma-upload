e1978b5692f7bca8a100e50929c8947edaeda51b
sai <sai@gmail.com>
2019-04-18 16:28:35 +0700

    delete unused files

--- KyarNiKanSayadaw/dl.sh
+++
@@ -1,4 +0,0 @@
-wget -c -O 096.wma "https://media.thitsarparamisociety.com/mp3/KyarNiKan/CD1/Kyar%20Ni%20Kan%20U%20Zadila%20-%20MaKongSoneThaw-MettaTaYar.wma" 
-wget -c -O 097.wma "https://media.thitsarparamisociety.com/mp3/KyarNiKan/CD1/Kyar%20Ni%20Kan%20U%20Zadila%20-%20PuHtuZin-NhatMyoKhwairChan.wma" 
-wget -c -O 098.wma "https://media.thitsarparamisociety.com/mp3/KyarNiKan/CD1/Kyar%20Ni%20Kan%20U%20Zadila%20-%20BelThuPyaiPyai-MaNaingTat-AhHluTayar.wma" 
-
