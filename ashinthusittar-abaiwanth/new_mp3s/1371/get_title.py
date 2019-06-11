d9593323694b039f2aa36ebf07d55cc3293e6b32
sai <sai@gmail.com>
2019-06-11 10:08:48 +0700

    init

---
+++ ashinthusittar-abaiwanth/new_mp3s/1371/get_title.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python
+# coding=utf-8
+
+def change(num):
+    mm = ['၀','၁','၂','၃', '၄', '၅', '၆','၇','၈','၉']
+    strmm = ''
+    mmlist = list(map(int, str(num)))
+
+    for i in mmlist:
+        strmm += mm[i]
+
+    return strmm
+
+title = '၁၃၇၁-ခု ႏွစ္ တြင္ ပုိ႕ ခ် ေတာ္ မူ အပ္ ေသာ နံ႕သာျမိဳင္ ပါရာဇိကဏ္ ပထမပါရာဇိက၀ါက်'
+
+for f in range(1, 200):
+    fmt = '{:03d}.mp3'.format(f)
+    print('{}|test|{}။{}'.format(fmt,change(f), title))
