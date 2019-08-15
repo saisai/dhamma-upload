b67ff3522bf2f4d65893a7d62aded2051d7b9a97
sai <sai@gmail.com>
2019-08-15 10:27:26 +0700

    init

---
+++ future/fb/www_upyinnyarnanda/added_new_url.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python
+# coding=utf-8
+
+from collections import Counter
+
+urls = [f.strip('\n') for f in open('new_raw_url.txt')]
+
+#print(urls)
+
+#print(Counter(urls).items())
+for key, val in Counter(urls).items():
+    if val > 1:
+        print(key, val)
---
+++ future/fb/www_upyinnyarnanda/readme.txt
@@ -0,0 +1,2 @@
+Added the newest links to the top of file of the raw_url.txt.
+Because these will go the bottom of the links.txt
