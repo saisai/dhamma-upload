cbd13cd43ad463b6d1e248909ba46316f87b7eb0
sai <sai@gmail.com>
2019-10-02 12:24:14 +0700

    init

---
+++ vocaroo_com.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python
+# coding=utf-8
+import re
+import glob
+from shutil import copy 
+# copyfile(src, dst)
+
+def atoi(text):
+    return int(text) if text.isdigit() else text
+
+def natural_keys(text):
+    return [ atoi(c) for c in re.split('(\d+)', text) ]
+
+files = glob.glob('*.MP3')
+
+count = 1
+for f in sorted(files, key=natural_keys):
+    tt = '{:03d}.mp3'.format(count)
+    dest = 'edit/{}'.format(tt)
+    print(tt)
+    print(dest)
+    print(f)
+    copy(f, dest)
+    count += 1
+
